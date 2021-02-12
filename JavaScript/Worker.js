390f4b032bf3ecb2e561cf05d7d72011bd504fcf
LacieBernkastel <xhimitsu.lacie@gmail.com>
2021-02-12 09:42:45 +0100

    Update

--- JavaScript/Worker.js
+++ JavaScript/Worker.js
@@ -1,10 +1,8 @@
-let intervalID = setInterval(withFetch, 1000, callback);
+let intervalID = setInterval(getNewCoordinates, 1000, callback);
 let point = new Point(50,50);
 
 onmessage = function(e) {
     console.log('Worker: Message received from main script');
-    point.x = getRandomIntInclusive(20, 580);
-    point.y = getRandomIntInclusive(20,500);
     const result = point;
     const workerResult = 'Result: ' + result;
     console.log('Worker: Posting message back to main script');
@@ -15,6 +13,9 @@ function getRandomIntInclusive(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min +1)) + min;
-  }
+}
 
-  
+function getNewCoordinates(){
+    point.x = getRandomIntInclusive(20, 580);
+    point.y = getRandomIntInclusive(20,500);
+}
