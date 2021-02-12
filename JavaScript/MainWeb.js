b0c79585ca0366398457ad3255fe3912fd58ea02
LacieBernkastel <xhimitsu.lacie@gmail.com>
2021-02-12 09:39:53 +0100

    TP4 test

--- JavaScript/MainWeb.js
+++ JavaScript/MainWeb.js
@@ -1,4 +1,3 @@
-let intervalID = window.setInterval(withFetch, 1000, callback);
 let canvas = document.getElementsByTagName("canvas")[0];
 let canvas2dContext = canvas.getContext("2d");
 const result = document.querySelector('.result');
@@ -8,9 +7,28 @@ if (window.Worker) {
 	const myWorker = new Worker("Worker.js");
 
 	myWorker.onmessage = function(e) {
-		result.textContent = e.data;
+		result.textContent = drawCircle(e.data);
 		console.log('Message received from worker');
 	}
+
 } else {
 	console.log('Your browser doesn\'t support web workers.')
+}
+
+function drawCircle(data){
+    
+    canvas2dContext.fillStyle = 'rgb(255,0,0)';
+    canvas2dContext.clearRect(0, 0, canvas.width, canvas.height);
+    canvas2dContext.beginPath();
+    canvas2dContext.arc(data.x, data.y, 20, 0, (2*Math.PI), false); 
+    canvas2dContext.fill();
+
+}
+
+class Point{
+
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
 }
--- JavaScript/Worker.js
+++ JavaScript/Worker.js
@@ -1,11 +1,20 @@
+let intervalID = setInterval(withFetch, 1000, callback);
+let point = new Point(50,50);
+
 onmessage = function(e) {
-  console.log('Worker: Message received from main script');
-  const result = e.data[0] * e.data[1];
-  if (isNaN(result)) {
-    postMessage('Please write two numbers');
-  } else {
+    console.log('Worker: Message received from main script');
+    point.x = getRandomIntInclusive(20, 580);
+    point.y = getRandomIntInclusive(20,500);
+    const result = point;
     const workerResult = 'Result: ' + result;
     console.log('Worker: Posting message back to main script');
     postMessage(workerResult);
-  }
 }
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+  }
+
+  
