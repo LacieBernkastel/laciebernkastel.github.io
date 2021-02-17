6e8de1204c3723de4ad34073729647d65fcf720f
LacieBernkastel <xhimitsu.lacie@gmail.com>
2021-02-17 12:12:05 +0100

    TP5 up to store in DB

--- JavaScript/Calendar.js
+++ JavaScript/Calendar.js
@@ -1,26 +1,210 @@
 const scheduleDisplay = document.querySelector('.scheduleDisplay');
+const list = document.querySelector('ul.scheduleDisplay');
+let db;
+
+window.onload = function() {
+
+    // Open our database; it is created if it doesn't already exist
+    // (see onupgradeneeded below)
+    let request = window.indexedDB.open('schedule_db', 1);
+
+    // onerror handler signifies that the database didn't open successfully
+    request.onerror = function() {
+        console.log('Database failed to open');
+    };
+
+    // onsuccess handler signifies that the database opened successfully
+    request.onsuccess = function() {
+        console.log('Database opened succesfully');
+
+        // Store the opened database object in the db variable. This is used a lot below
+        db = request.result;
+
+        // Run the displayData() function to display the notes already in the IDB
+        displayData();
+    };
+
+    // Setup the database tables if this has not already been done
+    request.onupgradeneeded = function(e) {
+
+        // Grab a reference to the opened database
+        let db = e.target.result;
+
+        // Create an objectStore to store our notes in (basically like a single table)
+        // including a auto-incrementing key
+        let objectStore = db.createObjectStore('schedule_course', { keyPath: 'id', autoIncrement:true });
+
+        // Define what data items the objectStore will contain
+        objectStore.createIndex('title', 'title', { unique: false });
+        objectStore.createIndex('startDate', 'startDate', { unique: true });
+        objectStore.createIndex('endDate', 'endDate', { unique: true });
+
+        console.log('Database setup complete');
+    };
+}
+
+
+    // Define the addData() function
+    function addData(e) {
+        // prevent default - we don't want the form to submit in the conventional way
+        // e.preventDefault();
+
+        // grab the values entered into the form fields and store them in an object ready for being inserted into the DB
+        let newItem = { title: e.summary, startDate: e.startDate.toJSDate(), endDate: e.endDate.toJSDate() };
+
+        // open a read/write db transaction, ready for adding the data
+        let transaction = db.transaction(['schedule_course'], 'readwrite');
+
+        // call an object store that's already been added to the database
+        let objectStore = transaction.objectStore('schedule_course');
+
+        // Make a request to add our newItem object to the object store
+        var request = objectStore.add(newItem);
+
+        // Report on the success of the transaction completing, when everything is done
+        transaction.oncomplete = function() {
+        console.log('Transaction completed: database modification finished.');
+
+        // update the display of data to show the newly added item, by running displayData() again.
+        displayData();
+        };
+
+        transaction.onerror = function() {
+        console.log('Transaction not opened due to error');
+        };
+    }
+
+        function displayData() {
+            // Here we empty the contents of the list element each time the display is updated
+            // If you ddn't do this, you'd get duplicates listed each time a new note is added
+            while (list.firstChild) {
+            list.removeChild(list.firstChild);
+            }
+        
+            // Open our object store and then get a cursor - which iterates through all the
+            // different data items in the store
+            let objectStore = db.transaction('schedule_course').objectStore('schedule_course');
+            objectStore.openCursor().onsuccess = function(e) {
+            // Get a reference to the cursor
+            let cursor = e.target.result;
+        
+            // If there is still another data item to iterate through, keep running this code
+            if(cursor) {
+                // Create a list item, h3, and p to put each data item inside when displaying it
+                // structure the HTML fragment, and append it inside the list
+                const listItem = document.createElement('li');
+                const h3 = document.createElement('h3');
+                const para = document.createElement('p');
+        
+                listItem.appendChild(h3);
+                listItem.appendChild(para);
+                list.appendChild(listItem);
+        
+                // Put the data from the cursor inside the h3 and para
+                h3.textContent = cursor.value.title;
+                para.textContent = cursor.value.startDate + cursor.value.endDate;
+        
+                // Store the ID of the data item inside an attribute on the listItem, so we know
+                // which item it corresponds to. This will be useful later when we want to delete items
+                listItem.setAttribute('data-course-id', cursor.value.id);
+        
+                // Create a button and place it inside each listItem
+                const deleteBtn = document.createElement('button');
+                listItem.appendChild(deleteBtn);
+                deleteBtn.textContent = 'Delete';
+        
+                // Set an event handler so that when the button is clicked, the deleteItem()
+                // function is run
+                deleteBtn.onclick = deleteItem;
+        
+                // Iterate to the next item in the cursor
+                cursor.continue();
+            } else {
+                // Again, if list item is empty, display a 'No notes stored' message
+                if(!list.firstChild) {
+                const listItem = document.createElement('li');
+                listItem.textContent = 'No courses stored.'
+                list.appendChild(listItem);
+                }
+                // if there are no more cursor items to iterate through, say so
+                console.log('Courses all displayed');
+            }
+            };
+        }
+
+    
+      // Define the deleteItem() function
+      function deleteItem(e) {
+        // retrieve the name of the task we want to delete. We need
+        // to convert it to a number before trying it use it with IDB; IDB key
+        // values are type-sensitive.
+        let noteId = Number(e.target.parentNode.getAttribute('data-course-id'));
+    
+        // open a database transaction and delete the task, finding it using the id we retrieved above
+        let transaction = db.transaction(['schedule_course'], 'readwrite');
+        let objectStore = transaction.objectStore('schedule_course');
+        let request = objectStore.delete(noteId);
+    
+        // report that the data item has been deleted
+        transaction.oncomplete = function() {
+          // delete the parent of the button
+          // which is the list item, so it is no longer displayed
+          e.target.parentNode.parentNode.removeChild(e.target.parentNode);
+          console.log('Note ' + noteId + ' deleted.');
+    
+          // Again, if list item is empty, display a 'No notes stored' message
+          if(!list.firstChild) {
+            const listItem = document.createElement('li');
+            listItem.textContent = 'No notes stored.';
+            list.appendChild(listItem);
+          }
+        };
+    }
+
+
 
 fetch('INFO2_G1_TP1.ics')
-    .then(response => response.text())
-    .then(createCalendar);
+        .then(response => response.text())
+        .then(createCalendar);
 
 
-function createCalendar(raw){
+    function createCalendar(raw){
 
-    // Get the basic data out
-    var jCalData = ICAL.parse(raw);
-    var comp = new ICAL.Component(jCalData);
+        // Get the basic data out
+        var jCalData = ICAL.parse(raw);
+        var comp = new ICAL.Component(jCalData);
 
-    // Fetch the VEVENT part
-    var vevent = comp.getFirstSubcomponent('vevent');
-    var event = new ICAL.Event(vevent); 
 
-    scheduleDisplay.textContent = event.summary + ' ' + event.description + ' ' + event.startDate.toJSDate() + ' ' + event.endDate.toJSDate();
+        // Fetch the VEVENT part
+        var vevent = comp.getAllSubcomponents('vevent');
+        vevent.sort((a,b) => {
+            var eventA = new ICAL.Event(a);
+            var eventB = new ICAL.Event(b);
 
-    console.log(event.summary, event.uid, event.description);
+            return eventA.startDate.toJSDate() - eventB.endDate.toJSDate();
+        } );
 
-    // Get start and end dates as local time on current machine
+        vevent.forEach(element => {
+                
+            var event = new ICAL.Event(element);
+            
+            if(event.startDate.toJSDate().toDateString() === new Date().toDateString()){
+                // let li = document.createElement('li');
 
-    console.log(event.startDate.toJSDate(), event.endDate.toJSDate());
+                // li.textContent = event.summary + ' ' + event.description + ' ' + event.startDate.toJSDate() + ' ' + event.endDate.toJSDate();
+                // scheduleDisplay.appendChild(li);
 
-}
+                addData(event);
+
+                console.log(event.summary, event.uid, event.description);
+
+                // Get start and end dates as local time on current machine
+
+                console.log(event.startDate.toJSDate(), event.endDate.toJSDate());
+            }
+
+            
+        });
+
+
+    }
